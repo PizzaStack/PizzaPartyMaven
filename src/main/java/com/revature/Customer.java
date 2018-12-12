89772daf69d1615423069f2cee8806fc214a9943
unknown <mehrab.rahman@revature.com>
2018-12-12 17:01:12 -0600

    First Commit

---
+++ src/main/java/com/revature/Customer.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+public class Customer {
+	private String name;
+
+	public Order makeOrder() {
+		return null;
+		
+	}
+
+	public String getsName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/revature/CustomerTest.java
@@ -0,0 +1,35 @@
+package com.revature;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CustomerTest {
+	
+	@Test
+	public void customerCanMakeOrder() {
+		Customer customer = new Customer();
+		customer.makeOrder();
+	}
+	
+	@Test
+	public void makeOrderWillCreateOrderObject() {
+		Customer customer = new Customer();
+		Order order = customer.makeOrder();
+	}
+	
+	@Test
+	public void customerGetsName() {
+		Customer customer = new Customer();
+		String testName = "Sam";
+		customer.setName(testName);
+		assertEquals(customer.getName(), testName);
+	}
+	
+	@Test
+	public void customerGetsOrderId() {
+		Customer customer = new Customer();
+		Order order = customer.makeOrder();
+		order.getId();
+	}
+}
