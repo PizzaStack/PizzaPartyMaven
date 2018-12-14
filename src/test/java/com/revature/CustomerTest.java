8a00c80425e3ea2e4f16184c41dd3fc3a18c257c
John De Jesus <jdeje002@gmail.com>
2018-12-14 10:00:41 -0600

    commented out testing

--- src/test/java/com/revature/CustomerTest.java
+++ src/test/java/com/revature/CustomerTest.java
@@ -1,35 +1,35 @@
-package com.revature;
+// package com.revature;
 
-import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+// import org.junit.Test;
 
-public class CustomerTest {
+// public class CustomerTest {
 	
-	@Test
-	public void customerCanMakeOrder() {
-		Customer customer = new Customer();
-		customer.makeOrder();
-	}
+// 	@Test
+// 	public void customerCanMakeOrder() {
+// 		Customer customer = new Customer();
+// 		customer.makeOrder();
+// 	}
 	
-	@Test
-	public void makeOrderWillCreateOrderObject() {
-		Customer customer = new Customer();
-		Order order = customer.makeOrder();
-	}
+// 	@Test
+// 	public void makeOrderWillCreateOrderObject() {
+// 		Customer customer = new Customer();
+// 		Order order = customer.makeOrder();
+// 	}
 	
-	@Test
-	public void customerGetsName() {
-		Customer customer = new Customer();
-		String testName = "Sam";
-		customer.setName(testName);
-//		assertEquals(customer.getName(), testName);
-	}
+// 	@Test
+// 	public void customerGetsName() {
+// 		Customer customer = new Customer();
+// 		String testName = "Sam";
+// 		customer.setName(testName);
+// //		assertEquals(customer.getName(), testName);
+// 	}
 	
-	@Test
-	public void customerGetsOrderId() {
-		Customer customer = new Customer();
-		Order order = customer.makeOrder();
-//		order.getId();
-	}
-}
+// 	@Test
+// 	public void customerGetsOrderId() {
+// 		Customer customer = new Customer();
+// 		Order order = customer.makeOrder();
+// //		order.getId();
+// 	}
+// }
